03baf828f3dd1e11aeff67799c622ef63c41a80a
Atrem Sergieiev <a_serg83@ukr.net>
2020-11-20 13:21:11 +0100

    Update task-8.js

--- js/task-8.js
+++ js/task-8.js
@@ -4,15 +4,21 @@ const galleryListRef = document.querySelector('.js-gallery');
 const lightboxImageRef = document.querySelector('.lightbox__image');
 const modalRef = document.querySelector('.js-lightbox');
 const closeModalBth = document.querySelector('[data-action="close-lightbox"]');
+const overlayRef = document.querySelector('.lightbox__overlay');
+const nextImgRef = document.querySelector('.lightbox__handler.next');
+const prevImgRef = document.querySelector('.lightbox__handler.prev');
 
 galleryListRef.insertAdjacentHTML('beforeend', getItemsMarkup(galleryItems));
 
 galleryListRef.addEventListener('click', handlerClickImages);
 closeModalBth.addEventListener('click', hendlerClosedModal);
+overlayRef.addEventListener('click', hendlerClosedModal);
+
+let indexImg;
 
 function getItemsMarkup(items) {
   return items.reduce(
-    (string, { preview, original, description }) =>
+    (string, { preview, original, description }, ind) =>
       string +
       `<li class="gallery__item">
          <a
@@ -23,6 +29,7 @@ function getItemsMarkup(items) {
           class="gallery__image"
           src="${preview}"
           data-source="${original}"
+          data-index="${ind}"
           alt="${description}"
           />
          </a>
@@ -37,20 +44,65 @@ function handlerClickImages(event) {
     return;
   }
   const imagesOriginal = event.target;
+  indexImg = Number(imagesOriginal.dataset.index);
 
   hendlerOpenModal();
   setImageOnModal(imagesOriginal);
 }
 
 function hendlerOpenModal() {
+  window.addEventListener('keydown', hendlerEscClosedModal);
+  window.addEventListener('keydown', hendlerModalGallary);
+  nextImgRef.addEventListener('click', scrolledNextImgOnModal);
+  prevImgRef.addEventListener('click', scrolledPrevImgOnModal);
   modalRef.classList.add('is-open');
 }
-function setImageOnModal(value) {
-  lightboxImageRef.src = value.dataset.source;
-  lightboxImageRef.alt = value.alt;
+
+function setImageOnModal(image) {
+  lightboxImageRef.src = image.dataset.source;
+  lightboxImageRef.alt = image.alt;
 }
 
+function hendlerModalGallary({ code }) {
+  if (code === 'ArrowRight') {
+    scrolledNextImgOnModal();
+  }
+  if (code === 'ArrowLeft') {
+    scrolledPrevImgOnModal();
+  }
+}
+
+function scrolledNextImgOnModal() {
+  if (indexImg < galleryItems.length - 1) {
+    indexImg += 1;
+    scrolledActiveImgOnModal(indexImg);
+  }
+}
+function scrolledPrevImgOnModal() {
+  if (indexImg > 0) {
+    indexImg -= 1;
+    scrolledActiveImgOnModal(indexImg);
+  }
+}
+
+function scrolledActiveImgOnModal(indexImg) {
+  const activeSrc = galleryItems[indexImg].original;
+  const activeAlt = galleryItems[indexImg].description;
+  lightboxImageRef.src = activeSrc;
+  lightboxImageRef.alt = activeAlt;
+}
+
+//==========functions close==========//
+function hendlerEscClosedModal({ code }) {
+  if (code === 'Escape') {
+    hendlerClosedModal();
+  }
+}
 function hendlerClosedModal() {
+  window.removeEventListener('keydown', hendlerEscClosedModal); //при закрытии снимаем слушатель
+  window.removeEventListener('keydown', hendlerModalGallary);
+  nextImgRef.removeEventListener('click', scrolledNextImgOnModal);
+  prevImgRef.removeEventListener('click', scrolledPrevImgOnModal);
   modalRef.classList.remove('is-open');
   clearAttributeImageOnModal();
 }
